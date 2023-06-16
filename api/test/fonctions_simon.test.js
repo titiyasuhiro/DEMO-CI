930ef7cf7dac8c44c75a4bcd3f2a865734759fd0
sr4mu <simon.ramu@gmail.com>
2023-06-16 13:21:41 +0200

    Ajout de la fonction multiply avec le test correspondant

--- api/test/fonctions_simon.test.js
+++ api/test/fonctions_simon.test.js
@@ -5,21 +5,21 @@ import multiply from '../src/app.js';
 describe('multiply', function() {
   it('devrait retourner un nombre positif', function() {
     const result = multiply(5, 9);
-    assert.equal(result, 45);
+    test.assert.equal(result, 45);
   });
 
   it('devrait retourner 0', function() {
     const result = multiply(5, 0);
-    assert.equal(result, 0);
+    test.assert.equal(result, 0);
   });
 
   it('devrait retourner un nombre négatif', function() {
     const result = multiply(5, -9);
-    assert.equal(result, -45);
+    test.assert.equal(result, -45);
   });
 
   it('devrait retourner une erreur si on essaie de multiplier des strings', function() {
-    assert.throws(() => {
+    test.assert.throws(() => {
       multiply("4", "9");
     }, TypeError);
   });
